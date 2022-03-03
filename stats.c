cac16bdee42570c2e0806d973d98b3ce2c13c972
NithyaThangaraja <100401279+NithyaThangaraja@users.noreply.github.com>
2022-03-03 16:54:17 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,43 +1,54 @@
-#include <stdio.h>
 #include "stats.h"
-#include "alert.h"
-#include <math.h>
 
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats statistics;
-    int i;
-    float sum = 0.0, max = numberset[0], min = numberset[0];
-    //Compute min, max and average
-	
-if(setlength!=0){
-    for (i=0; i<setlength; i++)
+    struct Stats s;
+    int i = 0;
+    
+    s.average = 0;
+    s.min = 0;
+    s.max = 0;
+      
+    if(setlength == 0)
     {
-        sum += numberset[i];
-        if(numberset[i] > max){
-		max = numberset[i];
-	}
-        if(numberset[i] < min){
-		min = numberset[i];
-	}
+        s.average = NAN;
+        s.min = NAN;
+        s.max = NAN;
+    }
+    else
+    {      
+        s.average = 0;
+        s.min = s.max = numberset[i];
+        
+        for (i=0; i<setlength; i++)
+        {
+            s.average += numberset[i];
+            if(numberset[i] < s.min) s.min = numberset[i];
+            if(numberset[i] > s.max) s.max = numberset[i];
+        }
+        s.average /= setlength;
     }
     
-    //Update the struct members   
-    statistics.average = sum/setlength;
-    statistics.min = min;
-    statistics.max = max;
-    } else{
-    statistics.average = NAN;
-    statistics.min = NAN;
-    statistics.max = NAN;
-    }
-    return statistics;  
+    return s;   
 }
 
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats){
-    //If the maximum value exceeded threshold, raise an alert
-	if(computedStats.max >  maxThreshold){
-		alerters[0]();
-		alerters[1]();
-	}
+void emailAlerter()
+{
+    emailAlertCallCount++;
+}
+
+void ledAlerter()
+{
+    ledAlertCallCount++;
+}
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    if (computedStats.max > maxThreshold)
+    {
+        alerters[0]();
+        alerters[1]();
+    }
 }
