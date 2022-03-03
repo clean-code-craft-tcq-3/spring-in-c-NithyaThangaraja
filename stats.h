3a126c486de2be38805e8d3140ebc55fd2d1464a
NithyaThangaraja <100401279+NithyaThangaraja@users.noreply.github.com>
2022-03-03 15:05:47 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,11 +1,11 @@
 
-void compute_statistics(const float* numberset, int setlength);
+//struct Stats compute_statistics(const float* numberset, int setlength);
 
-struct Stats
+struct Stats 
 {
-  int average; 
-  int min;
-  int max;
+    double average ;
+    float min ;
+    float max ;
 };
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
